4399f43a674d5c65cdf0773ac1031633f1d99dfc
nilSinger <niilz@outlook.de>
2021-07-17 21:24:51 +0200

    intermediate commit (contains lots of debug println!) but this version gets all fields and creates a template that gets filled with the fieldnames and values

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,43 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, parse::ParseStream, parse_macro_input, ItemStruct};
+use syn::{self, parse::ParseStream, parse_macro_input, Data, DataStruct, DeriveInput, Fields};
 
 #[proc_macro_derive(ToUrl)]
 pub fn to_url(tokens: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(tokens as ItemStruct);
+    let input = parse_macro_input!(tokens as DeriveInput);
+
+    let ast = format!("{:?}", input);
+    let ident_as_str = format!("{:?}", input.ident);
     let ident = input.ident;
+
+    let fields = match input.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => fields.named,
+        _ => panic!("Only structs with named fields can be annotated with ToUrl"),
+    };
+
+    let fields_as_str = format!("{:?}", fields);
+
+    let template: String = fields
+        .clone()
+        .iter()
+        .map(|f| f.ident.clone().unwrap().to_string() + "={}&")
+        .collect();
+
+    let pups = fields.iter().next().unwrap().clone().ident.unwrap();
+
     let modified = quote! {
         impl #ident {
-            fn demo(&self) {
-                println!("I have made macro");
+            fn to_url(&self, url: &str) {
+                println!("my ast: {:?}", #ast);
+                println!("my ident: {:?}", #ident_as_str);
+                println!("Those are my fields: {:?}", #fields_as_str);
+
+                println!("{}{}{}", url, #template, self.#pups);
+
             }
         }
     };
