cbab6eb499d8f1548532f8f2de67a09cd877b696
nilSinger <niilz@outlook.de>
2021-07-17 21:39:25 +0200

    intermediate commit where this version can be used on Reques (lifetimes are hardcoded though)

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ use syn::{self, parse::ParseStream, parse_macro_input, Data, DataStruct, DeriveI
 pub fn to_url(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as DeriveInput);
 
-    let ast = format!("{:?}", input);
-    let ident_as_str = format!("{:?}", input.ident);
-    let ident = input.ident;
+    let name = input.ident;
 
     let fields = match input.data {
         Data::Struct(DataStruct {
@@ -19,8 +17,6 @@ pub fn to_url(tokens: TokenStream) -> TokenStream {
         }) => fields.named,
         _ => panic!("Only structs with named fields can be annotated with ToUrl"),
     };
-
-    let fields_as_str = format!("{:?}", fields);
 
     let template: String = fields
         .clone()
@@ -31,13 +27,12 @@ pub fn to_url(tokens: TokenStream) -> TokenStream {
     let pups = fields.iter().next().unwrap().clone().ident.unwrap();
 
     let modified = quote! {
-        impl #ident {
-            fn to_url(&self, url: &str) {
-                println!("my ast: {:?}", #ast);
-                println!("my ident: {:?}", #ident_as_str);
-                println!("Those are my fields: {:?}", #fields_as_str);
+        impl<'a> #name<'a> {
+            pub fn to_url(&self, url: String) -> String {
 
                 println!("{}{}{}", url, #template, self.#pups);
+
+                "dummy".to_string()
 
             }
         }
